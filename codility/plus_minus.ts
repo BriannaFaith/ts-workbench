d588e75db6e4af167e3634775f1f42e8cd398701
Brianna Faith <faithbrianna01@gmail.com>
2025-02-21 20:51:46 +0300

    Interview coditility tests

---
+++ codility/plus_minus.ts
@@ -0,0 +1,28 @@
+// Given an array of integers,
+// calculate the ratios of its elements that are positive, negative, and zero.
+// Print the decimal value of each fraction on a new line with  places after the decimal.
+
+function plusMinus(arr: number[]): void {
+  let positive: number = 0;
+  let negative: number = 0;
+  let zero: number = 0;
+  let total: number = arr.length;
+
+  arr.forEach((num) => {
+    if (num > 0) {
+      positive++;
+    } else if (num < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  }
+  );
+
+  console.log((positive / total).toFixed(6));
+  console.log((negative / total).toFixed(6));
+  console.log((zero / total).toFixed(6));
+}
+
+let numbers: number[] = [-4, 3, -9, 0, 4, 1];
+plusMinus(numbers);
